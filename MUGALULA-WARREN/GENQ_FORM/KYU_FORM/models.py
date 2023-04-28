6b34f148bf307749663373a96ee6698724f7425a
WarrenMu <omnikkotow@gmail.com>
2023-04-28 11:33:10 +0300

    Making my form models

--- MUGALULA-WARREN/GENQ_FORM/KYU_FORM/models.py
+++ MUGALULA-WARREN/GENQ_FORM/KYU_FORM/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+
+#Bio Data
+class BioData(models.Model):
+    FirstName = models.CharField(max_length=25)
+    LastName = models.CharField(max_length=25)
+    date_of_birth = models.DateField()
+    def __str__(self):
+        return str(self.date_of_birth)
+    Male = 'M'
+    Female = 'F'
+    GenderChoices = [(Male, 'Male'), (Female, 'Female')]
+    GenderName = models.CharField(max_length=2, choices=GenderChoices)
+    def __str__(self):
+        return self.GenderName()
+
+#Location
+class Location(models.Model):
+    country = models.CharField(max_length=25)
+    StateOrDistrict = models.CharField(max_length=25)
+    Town = models.CharField(max_length=25)
+    ZipCode = models.IntegerField(max_length=6)
+    
+#Contacts and Address
+class ContactAndAddress(models.Model):
+    PhoneNumber1 = models.IntegerField(max_length=20)
+    PhoneNumber2 = models.IntegerField(max_length=20)
+    Email = models.CharField(max_length=20)
+    
+    
+    
+    
+    
