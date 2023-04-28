e8d146552a49d8e5f1128201e31d6dabc4b5fe28
WarrenMu <omnikkotow@gmail.com>
2023-04-28 13:13:17 +0300

    a working prototype without css yet

--- MUGALULA-WARREN/GENQ_FORM/KYU_FORM/models.py
+++ MUGALULA-WARREN/GENQ_FORM/KYU_FORM/models.py
@@ -7,26 +7,22 @@ class BioData(models.Model):
     FirstName = models.CharField(max_length=25)
     LastName = models.CharField(max_length=25)
     date_of_birth = models.DateField()
-    def __str__(self):
-        return str(self.date_of_birth)
     Male = 'M'
     Female = 'F'
     GenderChoices = [(Male, 'Male'), (Female, 'Female')]
     GenderName = models.CharField(max_length=2, choices=GenderChoices)
-    def __str__(self):
-        return self.GenderName()
 
 #Location
 class Location(models.Model):
     country = models.CharField(max_length=25)
     StateOrDistrict = models.CharField(max_length=25)
     Town = models.CharField(max_length=25)
-    ZipCode = models.IntegerField(max_length=6)
+    ZipCode = models.IntegerField()
     
 #Contacts and Address
 class ContactAndAddress(models.Model):
-    PhoneNumber1 = models.IntegerField(max_length=20)
-    PhoneNumber2 = models.IntegerField(max_length=20)
+    PhoneNumber1 = models.IntegerField()
+    PhoneNumber2 = models.IntegerField()
     Email = models.CharField(max_length=20)
     
     
--- MUGALULA-WARREN/GENQ_FORM/KYU_FORM/views.py
+++ MUGALULA-WARREN/GENQ_FORM/KYU_FORM/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def registration(request):
+    # FirstName = request.POST['FirstName']
+    # LastName = request.POST['LastName']
+    # date_of_birth = request.POST['date_of_birth']
+    # GenderName = request.POST['GenderName']
+    # country = request.POST['country']
+    # StateOrDistrict = request.POST['StateOrDistrict']
+    # Town = request.POST['Town']
+    # ZipCode = request.POST['ZipCode']
+    # PhoneNumber1 = request.POST['PhoneNumber1']
+    # PhoneNumber2 = request.POST['PhoneNumber2']
+    # Email = request.POST['Email']
+    # if BioData.objects.filter(username=FirstName).first():
+    #     print('Name already exists.')
+    #     return render(request, 'register.html')
+    # else:
+    #     return False
+    return render(request, 'register.html')
+    
